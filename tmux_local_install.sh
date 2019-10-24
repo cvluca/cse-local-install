702403f329c8a0bb9ebfc04594553fe760f1207e
z5155740 <z5155740@unsw.edu.au>
2019-10-24 20:21:41 +1100

    update tmux build

--- tmux_local_install.sh
+++ tmux_local_install.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
 
 # Script for installing tmux on systems where you don't have root access.
-# tmux will be installed in $HOME/.local/usr/local/bin.
+# tmux will be installed in $HOME/bin.
 # It's assumed that wget and a C/C++ compiler are installed.
 
 # exit on error
 set -e
 
-TMUX_VERSION=2.5
+TMUX_VERSION=2.9a
+LIBEVENT_VERSION=2.1.11
+BUILD_DIR=/tmp/tmux_build-$USER
+BIN_DIR=$HOME/bin
 
 # create our directories
-mkdir -p $HOME/.local/usr/local $HOME/tmux_tmp
-cd $HOME/tmux_tmp
-
-# download source files for tmux, libevent, and ncurses
-wget -O tmux-${TMUX_VERSION}.tar.gz https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
-wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
-wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz
-
-# extract files, configure, and compile
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
 
 ############
 # libevent #
 ############
-tar xvzf libevent-2.1.8-stable.tar.gz
-cd libevent-2.1.8-stable
-./configure --prefix=$HOME/.local/usr/local --disable-shared
-make
-make install
-cd ..
-
-############
-# ncurses  #
-############
-tar xvzf ncurses-5.9.tar.gz
-cd ncurses-5.9
-./configure --prefix=$HOME/.local/usr/local
+wget https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}-stable/libevent-${LIBEVENT_VERSION}-stable.tar.gz
+tar xvzf libevent-${LIBEVENT_VERSION}-stable.tar.gz
+cd libevent-${LIBEVENT_VERSION}-stable
+./configure --prefix=$BUILD_DIR --disable-shared
 make
 make install
 cd ..
@@ -43,18 +30,15 @@ cd ..
 ############
 #   tmux   #
 ############
+wget -O tmux-${TMUX_VERSION}.tar.gz https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
 tar xvzf tmux-${TMUX_VERSION}.tar.gz
 cd tmux-${TMUX_VERSION}
-./configure CFLAGS="-I$HOME/.local/usr/local/include -I$HOME/.local/usr/local/include/ncurses" LDFLAGS="-L$HOME/.local/usr/local/lib -L$HOME/.local/usr/local/include/ncurses -L$HOME/.local/usr/local/include"
-CPPFLAGS="-I$HOME/.local/usr/local/include -I$HOME/.local/usr/local/include/ncurses" LDFLAGS="-static -L$HOME/.local/usr/local/include -L$HOME/.local/usr/local/include/ncurses -L$HOME/.local/usr/local/lib" make
-cp tmux $HOME/.local/usr/local/bin
+./configure CFLAGS="-I$BUILD_DIR/include" LDFLAGS="-L$BUILD_DIR/lib -L$BUILD_DIR/include"
+CPPFLAGS="-I$BUILD_DIR/include" LDFLAGS="-static -L$BUILD_DIR/include -L$BUILD_DIR/lib" make
+cp tmux $BIN_DIR
 cd ..
 
 # cleanup
-rm -rf $HOME/tmux_tmp
-
-#copy to ~/bin
-rm ~/bin/tmux
-cp ~/.local/usr/local/bin/tmux ~/bin
+rm -rf $BUILD_DIR
 
 echo "tmux ${TMUX_VERSION} is now available."
